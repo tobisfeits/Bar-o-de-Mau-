6327c57367d0acf638966838e969c8783894e682
Tobias Matos <tobias.feitosa@gmail.com>
2026-03-02 15:22:45 -0300

    fix: force deployment by bumping version to v6 and fixing encodings

--- src/modules/version-checker.js
+++ src/modules/version-checker.js
@@ -1,5 +1,5 @@
 export const VersionChecker = {
-    currentVersion: '2026.03.02.005',
+    currentVersion: '2026.03.02.006_FINAL_FIX',
     checkInterval: 30000, // 30 segundos
     intervalId: null,
 
